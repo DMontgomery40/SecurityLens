7ef0ad6f15adf704e81d385e6678aada12f37bf9
David Montgomery <130489651+DMontgomery40@users.noreply.github.com>
2024-11-26 14:19:51 -0700

    Update vite config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/plugin-vulnerability-scanner/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
